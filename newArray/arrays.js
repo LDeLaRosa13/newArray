8d8b1f98b6ced4acd7505ed7e88b82e8e0090f08
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-03-01 20:07:08 -0500

    Show methods in arrays

--- newArray/arrays.js
+++ newArray/arrays.js
@@ -1,3 +1,15 @@
 var drinks = ["pepsi", "coke", "sprite", "rootbeer"];
+//the .pop method will remove the rootbeer element from this array
+drinks.pop();
+console.log(drinks);
+
+
 var numbers = ["11", "12", "13", "18"];
+//the .shift method will remove the element in the [0] placement
+numbers.shift();
+console.log(numbers);
+
 var thoughts = ["true", "false", "false", "true"];
+//the .push method would return the array back to its original length
+thoughts.push();
+console.log(thoughts);
